886b5edea760054b8dd6a3417bca59b7a8772bc1
mrjvs <jellevs@gmail.com>
2026-08-23 15:28:07 +0200

    feat: add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+const config = useRuntimeConfig();
+</script>
+
+<template>
+  <Container>
+    <div class="footer-container">
+      <div class="line" />
+      <div class="footer-content">
+        <NuxtLink
+          :href="config.public.homepageUrl"
+          class="brand"
+        >
+          Pretendo network
+        </NuxtLink>
+      </div>
+      <div class="line" />
+    </div>
+  </container>
+</template>
+
+<style lang="css" scoped>
+.footer-container {
+	display: flex;
+	gap: 1rem;
+	align-items: center;
+	margin-top: 8rem;
+	margin-bottom: 2rem;
+}
+
+.line {
+	flex: 1;
+	height: 1px;
+	background-color: var(--bg-shade-4);
+}
+
+.brand, .brand:visited {
+	color: var(--text-shade-3);
+	font-size: 0.8rem;
+	text-decoration: none;
+	cursor: pointer;
+	transition: color 100ms ease-in-out;
+}
+.brand:hover, .brand:focus {
+	color: var(--text-shade-1);
+	text-decoration: none;
+}
+</style>
