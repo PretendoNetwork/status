8d3004f3c3faadedd33ffda8d1e65d0a4426625b
mrjvs <jellevs@gmail.com>
2026-08-23 02:17:37 +0200

    feat: add incident viewer

---
+++ src/components/Incident.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { Incident } from '~~/shared/types';
+
+const props = defineProps<{
+	incident: Incident;
+}>();
+</script>
+
+<template>
+  <div class="incident">
+    <p>Status: {{ props.incident.resolvedAt ? 'Resolved' : 'Ongoing' }}</p>
+    <div
+      v-for="post of props.incident.posts"
+      :key="post.id"
+      class="incident-post"
+    >
+      <h1>{{ post.title }}</h1>
+      <p v-if="post.body">
+        {{ post.body }}
+      </p>
+    </div>
+  </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,25 +8,43 @@ const { data: status, isFetching, suspense } = useQuery({
 	refetchInterval: 15 * 1000
 });
 
+const { data: incidents, suspense: suspenseIncidents } = useQuery({
+	queryKey: ['incidents'],
+	queryFn: () => $fetch('/api/incidents'),
+	refetchInterval: 60 * 1000
+});
+
 onServerPrefetch(async () => {
 	await suspense();
+	await suspenseIncidents();
 });
 
 const globalStatus = computed(() => !(status.value?.services ?? []).some(v => !v.healthy));
 </script>
 
 <template>
-  <Container>
-    <GlobalStatus :healthy="globalStatus" />
-    <p v-if="isFetching">
-      Fetching...
-    </p>
-    <ServiceGroup>
-      <Service
-        v-for="svc of (status?.services ?? [])"
-        :key="svc.id"
-        :service="svc"
-      />
-    </ServiceGroup>
-  </Container>
+  <div>
+    <Container>
+      <GlobalStatus :healthy="globalStatus" />
+      <p v-if="isFetching">
+        Fetching...
+      </p>
+      <ServiceGroup>
+        <Service
+          v-for="svc of (status?.services ?? [])"
+          :key="svc.id"
+          :service="svc"
+        />
+      </ServiceGroup>
+    </Container>
+    <Container>
+      <IncidentList>
+        <Incident
+          v-for="incident of (incidents?.data ?? [])"
+          :key="incident.id"
+          :incident="incident"
+        />
+      </IncidentList>
+    </Container>
+  </div>
 </template>
